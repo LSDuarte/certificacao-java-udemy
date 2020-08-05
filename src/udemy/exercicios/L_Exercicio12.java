082412d23448ab340e6c968d98f980f2a6c0244a
Larissa Duarte <42499@pjmt.local>
2020-08-05 16:24:03 -0400

    05/08/2020 - Iniciando e encerrando Loops

---
+++ src/udemy/exercicios/L_Exercicio12.java
@@ -0,0 +1,21 @@
+package udemy.exercicios;
+
+/**
+ * @author LSDuarte
+ *	
+ *	Pergunta 12: Qual a saida?
+ *
+ *	1 (X)
+ *	Nada
+ *	Erro de compilação
+ *	Exception
+ */
+public class L_Exercicio12 {
+
+	public static void main(String[] args) {
+		do {
+			System.out.println("1");
+		} while (false);
+	}
+
+}
