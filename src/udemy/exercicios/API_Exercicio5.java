80290a1c717898780c417af83b0f5db75daf3c06
Larissa Duarte <42499@pjmt.local>
2020-08-06 17:52:40 -0400

    06/08/2020 - Inicio e Fim do conteudo sobre API JAVA - Faltam Simulados Apenas Para finalizacao do curso.

---
+++ src/udemy/exercicios/API_Exercicio5.java
@@ -0,0 +1,21 @@
+package udemy.exercicios;
+
+/**
+ * @author LSDuarte
+ * 
+ *  Pergunta 5: Quantos objetos String foram criados no trecho de código acima?
+ *  
+ *  1
+ *  2
+ *  3
+ *  4 (X)
+ *  5
+ */
+public class API_Exercicio5 {
+
+	public static void main(String[] args) {
+		String a = "Curso"; // foi criada a string CURSO
+		a.concat("Java"); // criada a string JAVA
+		a.toLowerCase(); //  criada a string curso minusculo 
+	}
+}
